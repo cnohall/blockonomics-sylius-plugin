109f120d4359a36a953f93f8826e8dad56ea49c1
cnohall <christopher.nohall@gmail.com>
2024-09-24 11:39:27 +0900

    redirect correctly to html.twig page

---
+++ src/Action/ObtainCardHolderAuthenticationAction.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file has been created by developers from BitBag.
+ * Feel free to contact us once you face any issues or want to start
+ * You can find more information about us on https://bitbag.io and write us
+ * an email on hello@bitbag.io.
+ */
+
+declare(strict_types=1);
+
+namespace Blockonomics\SyliusBlockonomicsPlugin\Action;
+
+use Payum\Core\Action\ActionInterface;
+use Payum\Core\Bridge\Spl\ArrayObject;
+use Payum\Core\Exception\RequestNotSupportedException;
+use Payum\Core\GatewayAwareInterface;
+use Payum\Core\GatewayAwareTrait;
+use Payum\Core\Reply\HttpResponse;
+use Payum\Core\Request\GetHttpRequest;
+use Payum\Core\Request\RenderTemplate;
+
+final class ObtainCardholderAuthenticationAction implements ActionInterface, GatewayAwareInterface
+{
+    use GatewayAwareTrait;
+
+    private string $templateName;
+
+    public function __construct(string $templateName)
+    {
+        $this->templateName = $templateName;
+    }
+
+    public function execute($request): void
+    {
+        RequestNotSupportedException::assertSupports($this, $request);        
+
+        $this->gateway->execute($template = new RenderTemplate($this->templateName, [
+            'formAction' => '',
+        ]));
+
+        throw new HttpResponse($template->getResult());
+    }
+
+    public function supports($request): bool
+    {
+        return true;
+    }
+}
--- src/BlockonomicsGatewayFactory.php
+++ src/BlockonomicsGatewayFactory.php
@@ -26,67 +26,22 @@ final class BlockonomicsGatewayFactory extends GatewayFactory
         $config->defaults([
             'payum.factory_name' => self::FACTORY_NAME,
             'payum.factory_title' => 'blockonomics',
-
             'payum.http_client' => '@blockonomics_sylius_blockonomics_plugin.api_client.blockonomics',
-
-            'payum.template.obtain_payment_method_nonce' => '@BlockonomicsSyliusBlockonomicsPlugin/Action/obtain_payment_method_nonce.html.twig',
             'payum.template.obtain_cardholder_authentication' => '@BlockonomicsSyliusBlockonomicsPlugin/Action/obtain_cardholder_authentication.html.twig',
-
-            'payum.action.capture' => new CaptureAction(),
-
+            'payum.action.capture' => function (ArrayObject $config) {
+                return new ObtainCardholderAuthenticationAction($config['payum.template.obtain_cardholder_authentication']);
+            },
             'payum.action.purchase' => function (ArrayObject $config) {
-                $action = new PurchaseAction();
-                $action->setCardholderAuthenticationRequired($config['cardholderAuthenticationRequired']);
-
-                return $action;
+                return new ObtainCardholderAuthenticationAction($config['payum.template.obtain_cardholder_authentication']);
             },
-
-            'payum.action.convert_payment' => new ConvertPaymentAction(),
-
-            'payum.action.obtain_payment_method_nonce' => function (ArrayObject $config) {
-                $action = new ObtainPaymentMethodNonceAction($config['payum.template.obtain_payment_method_nonce']);
-                $action->setCardholderAuthenticationRequired($config['cardholderAuthenticationRequired']);
-
-                return $action;
-            },
-
             'payum.action.obtain_cardholder_authentication' => function (ArrayObject $config) {
                 return new ObtainCardholderAuthenticationAction($config['payum.template.obtain_cardholder_authentication']);
             },
-
-            'payum.action.status' => new StatusAction(),
-
-            'payum.action.api.generate_client_token' => new GenerateClientTokenAction(),
-            'payum.action.api.find_payment_method_nonce' => new FindPaymentMethodNonceAction(),
-            'payum.action.api.do_sale' => new DoSaleAction(),
-
+            'payum.action.status' => function (ArrayObject $config) {
+                return new ObtainCardholderAuthenticationAction($config['payum.template.obtain_cardholder_authentication']);
+            },
             'cardholderAuthenticationRequired' => true,
         ]);
-
-        if (false == $config['payum.api']) {
-            $config['payum.default_options'] = [
-                'sandbox' => true,
-                'storeInVault' => null,
-                'storeInVaultOnSuccess' => null,
-                'storeShippingAddressInVault' => null,
-                'addBillingAddressToPaymentMethod' => null,
-            ];
-            $config->defaults($config['payum.default_options']);
-            $config['payum.required_options'] = [];
-
-            $config['payum.api'] = function (ArrayObject $config) {
-//                $config->validateNotEmpty($config['payum.required_options']);
-//
-//                return new Api((array) $config, $config['payum.http_client'], $config['httplug.message_factory']);
-
-                /** @var BlockonomicsApiClientInterface $apiClient */
-                $apiClient = $config['payum.http_client'];
-
-                $apiClient->initialise((array) $config);
-
-                return $apiClient;
-            };
-        }
 
         $config['payum.paths'] = [
             'PayumBlockonomics' => __DIR__ . '/Resources/views',
